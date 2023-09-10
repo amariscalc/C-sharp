31468780dcaecf65b4ada619645d555eb027b251
amariscalc <alberto.mariscal.c@gmail.com>
2023-09-10 17:48:50 +0200

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,5 +2,19 @@
  * 2023/09/10
  */
 
-// print a message to console output
-Console.WriteLine("Hello, World!");
+// Import context execution
+using System;
+
+// Create a namespace
+namespace CSharpHelloWorld
+{
+    // We can compilate and execute this file without execute like a proyect. We should be create a class.
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // print a message to console output
+            Console.WriteLine("Hello, World!!!");
+        }
+    }
+}
