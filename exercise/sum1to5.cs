2b7b11f67ffab8bee8f224806281edd53feeaab8
Alberto A. Mariscal <36934800+amariscalc@users.noreply.github.com>
2023-10-07 13:14:19 +0200

    Create sum1to5.cs

---
+++ exercise/sum1to5.cs
@@ -0,0 +1,15 @@
+/*
+Sumar los numeros del 1 al 5
+*/
+// Declarar e inicializar las variables
+int i = 0, number=0;
+
+// Recorrer un loop for para sumar el siguiente número en cada vuelta
+for (i=1; i<=5; i++)
+{
+    number += i;
+
+}
+
+// Mostrar por pantalla el resultado
+Console.WriteLine($"Result :{number}");
